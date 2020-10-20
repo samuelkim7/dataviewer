895fb79618b263c23a8696d1aee1854bc4942fca
Samuel <samuelmwkim@gmail.com>
2020-10-20 15:57:57 +0900

    arranging view files

---
+++ src/main/java/com/sam/dataviewer/service/OrderService.java
@@ -0,0 +1,17 @@
+package com.sam.dataviewer.service;
+
+import com.sam.dataviewer.repository.MemberRepository;
+import com.sam.dataviewer.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+
+}
