3415483d9fd1ff0e1c1ca1472f6c111996db4295
Samuel <samuelmwkim@gmail.com>
2021-01-06 18:42:49 +0900

    final touch

--- src/test/java/com/sam/dataviewer/adminController/AdminHomeControllerTest.java
+++ src/test/java/com/sam/dataviewer/adminController/AdminHomeControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,8 +25,19 @@ class AdminHomeControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/admin"))
+        MockHttpServletResponse response = mockMvc.perform(get("/admin"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("admin/index"));
+                .andExpect(view().name("admin/index"))
+                .andReturn().getResponse();
+
+
+        response.getContentAsString().contains("DataViewer Admin");
+    }
+
+    @Test
+    public void aboutTest() throws Exception {
+        mockMvc.perform(get("/admin/about"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/about"));
     }
 }
