26492f8c2e17034768d08b6f01fb58ddfd8e715f
Samuel <samuelmwkim@gmail.com>
2021-01-11 22:59:54 +0900

    error handling completed

--- src/main/java/com/sam/dataviewer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sam/dataviewer/exception/GlobalExceptionHandler.java
@@ -1,25 +1,25 @@
 package com.sam.dataviewer.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestControllerAdvice
+@ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity handleCustomException(CustomException e, RedirectAttributes redirectAttributes) {
+    public String handleCustomException(CustomException e, Model model) {
+        model.addAttribute("error", e.getMessage());
         log.error("handleCustomException", e);
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return "error";
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
+    public String handleException(Exception e, Model model) {
+        model.addAttribute("error", e.getMessage());
         log.error("handleException", e);
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return "error";
     }
 }
